6ed9463ac82bb5a5dff6391f96e41911d0c0fa1a
Andrii <girskiy.andrey@gmail.com>
2023-05-26 14:08:13 +0300

    wesockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ChatGetaway } from './chat.getaway';
 import { TypeOrmModule } from './db/typeorm.module';
 
 @Module({
   imports: [TypeOrmModule, UsersModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [ChatGetaway],
 })
 export class AppModule {}
