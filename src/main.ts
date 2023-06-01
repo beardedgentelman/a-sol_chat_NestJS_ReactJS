2b971bf0f7fe4b2754727ac33e542a5b190be57b
Andrii <girskiy.andrey@gmail.com>
2023-06-01 11:30:40 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import * as express from 'express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 8080;
@@ -9,10 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
     credentials: true,
-    origin: /http:\/\/localhost:3000\//i,
   });
-  app.use('../uploads', express.static(join(__dirname, '..', 'uploads')));
 
   await app.listen(PORT);
 }
