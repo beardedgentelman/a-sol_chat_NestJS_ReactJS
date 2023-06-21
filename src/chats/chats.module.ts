04adf2befe9063db2ae8684440102a417bfb2193
beardedgentelman <girskiy.andrey@gmail.com>
2023-06-21 16:58:12 +0300

    user service

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from 'src/users/user.repository';
 import { ChatRepository } from './chat.repository';
 import { ChatsController } from './chats.controller';
 import { ChatsService } from './chats.service';
 
 @Module({
-  imports: [ChatRepository, UserRepository],
+  imports: [
+    ChatRepository,
+    UserRepository,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (ConfigService: ConfigService) => {
+        return {
+          secret: ConfigService.get('SECRET_KEY'),
+          signOptions: { expiresIn: ConfigService.get('EXPIRES_IN') },
+        };
+      },
+    }),
+  ],
   providers: [ChatsService],
   controllers: [ChatsController],
 })
